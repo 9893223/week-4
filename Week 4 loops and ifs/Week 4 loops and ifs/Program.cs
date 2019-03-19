4f9aa4c045ae3986d7857a706cced95025a29771
Benjamin De Rijk <9893223@student.toiohomai.ac.nz>
2019-03-20 11:47:26 +1300

    ok

---
+++ Week 4 loops and ifs/Week 4 loops and ifs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4_loops_and_ifs
+{
+    class Program
+  
+    {
+        static void Main(string[] args)
+        {
+            int counter = 76;
+            int finish = 46;
+
+           
+
+            while (counter >= finish)
+            {
+                if (counter % 2 != 0)
+                    Console.WriteLine(counter); counter--;
+            }
+
+           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ task 2 loops and ifs/task 2 loops and ifs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2_loops_and_ifs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            Console.WriteLine("Please enter a character or symbol i.e A,$,&:");
+            char character = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Thanks, now please enter the number of times you wish to see your symbol:");
+            int user = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= user; i++)
+
+            { 
+                Console.WriteLine(character); 
+            }
+
+            Console.ReadLine();
+        }
+
+     }
+}
